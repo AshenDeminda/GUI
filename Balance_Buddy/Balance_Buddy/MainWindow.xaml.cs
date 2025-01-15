2a1d2e96265b0d32616fda6ba961a831e12992c3
AshenDeminda <114336796+AshenDeminda@users.noreply.github.com>
2025-01-15 20:27:24 +0530

    Good Wpf

---
+++ Balance_Buddy/Balance_Buddy/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System.Configuration;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Balance_Buddy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NavigateDashboard_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Dashboard());
+        }
+
+        private void NavigateAddRecord_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new AddRecord());
+        }
+
+        private void NavigateReports_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Reports());
+        }
+
+        private void NavigateSettings_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new Settings());
+        }
+    }
+}
