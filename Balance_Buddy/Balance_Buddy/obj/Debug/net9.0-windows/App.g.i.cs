8ae18769964c0916265402f41f354d0deea080be
AshenDeminda <114336796+AshenDeminda@users.noreply.github.com>
2025-02-22 13:22:14 +0530

    Desktop

--- Balance_Buddy/Balance_Buddy/obj/Debug/net9.0-windows/App.g.i.cs
+++ Balance_Buddy/Balance_Buddy/obj/Debug/net9.0-windows/App.g.i.cs
@@ -60,7 +60,7 @@ namespace Balance_Buddy {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/Balance_Buddy;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Balance_Buddy;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
--- expense_tracker/src/Pages/Dashboard.jsx
+++ expense_tracker/src/Pages/Dashboard.jsx
@@ -173,5 +173,3 @@ const Dashboard = ({ }) => {
 };
 
 export default Dashboard;
-
-
